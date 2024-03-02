4e9f45aa4d7c8ac313ee46dab7e32a3d67541060
Shubham More <shubhamdmore03@gmail.com>
2024-03-03 00:10:30 +0530

    created home component

--- practice/src/app/components/home/home.component.ts
+++ practice/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { tick } from '@angular/core/testing';
 
 @Component({
   selector: 'app-home',
@@ -12,6 +13,7 @@ export class HomeComponent {
     {
         id: 1,
         name: 'John Doe',
+        url: '../../../assets/Images/profile1.png',
         active: true,
         age: 30,
         department: 'Human Resources',
@@ -20,7 +22,8 @@ export class HomeComponent {
     {
         id: 2,
         name: 'Jane Smith',
-        active: false,
+        url: '../../../assets/Images/profile2.jpeg',
+        active: true,
         age: 28,
         department: 'Marketing',
         salary: 55000
@@ -28,7 +31,8 @@ export class HomeComponent {
     {
         id: 3,
         name: 'Michael Johnson',
-        active: true,
+        url: '../../../assets/Images/profile3.png',
+        active: false,
         age: 35,
         department: 'Finance',
         salary: 60000
@@ -36,6 +40,7 @@ export class HomeComponent {
     {
         id: 4,
         name: 'Emily Brown',
+        url: '../../../assets/Images/profile2.jpeg',
         active: false,
         age: 40,
         department: 'Engineering',
@@ -44,13 +49,39 @@ export class HomeComponent {
     {
         id: 5,
         name: 'David Lee',
+        url: '../../../assets/Images/profile1.png',
         active: true,
         age: 45,
         department: 'Operations',
         salary: 75000
     }
 ];
+changeToActive(emp:any){
+  if(emp.active !== true){
+    emp.active = true;
+  }
+}
 
+changeToInactive(emp:any){
+  if(emp.active !== false){
+    emp.active = false;
+  }
+}
 
+onSubmit(id:any, name:any, age:any, department:any, salary:any){
+  this.employee.push({
+    id: id,
+    name: name,
+    url: '../../../assets/Images/profile2.jpeg',
+    active: false,
+    age: age,
+    department: department,
+    salary: salary
+  })
+}
+
+deletEmployee(i:number){
+  this.employee.splice(i,1);
+}
 
 }
