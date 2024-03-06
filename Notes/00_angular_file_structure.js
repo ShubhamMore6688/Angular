78645988be376382715a2361b658528f0697d6a0
Shubham More <shubhamdmore03@gmail.com>
2024-03-06 06:53:26 +0530

    angular file structure

---
+++ Notes/00_angular_file_structure.js
@@ -0,0 +1,13 @@
+// .editorconfig this file is used to setup the environment for devlopment as each developer has different coding 
+// styles to avoid the massy code we can setup the standards for entire the devlopment 
+// devoploper don't have access to this file this is reponsibility  of mananger or team lead
+
+
+// angular.json most imp file it contains all the project imformation. all configuration related to our project
+
+// tsconfig.json this is configuration file for the typescript compiler
+
+
+//src->app  --> component, services
+
+//assets  --> images, music  (this folder is publically accessible)
