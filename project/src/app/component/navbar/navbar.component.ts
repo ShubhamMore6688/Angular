78917a042547081405269b4afcf62f1069149ee6
Shubham More <shubhamdmore03@gmail.com>
2024-03-13 18:23:31 +0530

    viewchild for dom element

--- project/src/app/component/navbar/navbar.component.ts
+++ project/src/app/component/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -15,11 +15,19 @@ export class NavbarComponent {
   @Output()
   sendSearchText: EventEmitter<string> = new EventEmitter<string>()
 
+  //using this we can access the DOM element without waiting for any event to be happen
+
+  //this give two arguments 
+  // 1.read: use it to  read different token from the queried elements.
+  // 2.static: Determine when the query is resolved.
+      //true: when view is initialized (before the first change detection) for the first time.
+      //false: if you want it to be resolved after every change detection
+  @ViewChild('searchText', {static: false}) inputString: ElementRef
   searchText: string = '';
   
-  onInputText(item: HTMLInputElement){
+  onInputText(){
     // console.log(this.searchText)
-    this.searchText = item.value;
+    this.searchText = this.inputString.nativeElement.value;
     // after pressing search button then only the search in user list is perform
     this.sendSearchText.emit(this.searchText)
   }
