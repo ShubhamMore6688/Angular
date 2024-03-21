7afc93861fc069be382a287a6d0e0a58ef2fa6fc
Shubham More <shubhamdmore03@gmail.com>
2024-03-21 10:39:03 +0530

    ngoninit

--- project/src/app/component/ngonchange/ngonchange.component.ts
+++ project/src/app/component/ngonchange/ngonchange.component.ts
@@ -1,16 +1,24 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component, ElementRef, Input, OnChanges, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-ngonchange',
   templateUrl: './ngonchange.component.html',
   styleUrls: ['./ngonchange.component.scss']
 })
-export class NgonchangeComponent implements OnChanges {
+export class NgonchangeComponent implements OnChanges, OnInit {
   @Input() message: string;
-  
+  @ViewChild('para') temppara: ElementRef
   // ngonchanges is run only when the value of input variable is changes
   ngOnChanges(){
     console.log("ngonchange occure")
-    console.log(this.message)
+    // console.log(this.message)
+  }
+
+  //ngoninit is called only once(during the first change detection life cycle) after the ngonchange
+  // any property decorated with @viewchild, @viewchildren, @contentchild or @contentchildren will not be available to use
+  ngOnInit(){
+    console.log("ngoninit")
+    // we cannot access this
+    // console.log(this.temppara.nativeElement)
   }
 }
