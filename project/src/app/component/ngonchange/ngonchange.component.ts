e26cb33307159a65e288ac122ac5681c28f304ae
Shubham More <shubhamdmore03@gmail.com>
2024-03-22 08:42:20 +0530

    ngaftercontentinit hook

--- project/src/app/component/ngonchange/ngonchange.component.ts
+++ project/src/app/component/ngonchange/ngonchange.component.ts
@@ -1,16 +1,16 @@
-import { Component, DoCheck, ElementRef, Input, OnChanges, OnInit, ViewChild } from '@angular/core';
+import { AfterContentInit, Component, ContentChild, DoCheck, ElementRef, Input, OnChanges, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-ngonchange',
   templateUrl: './ngonchange.component.html',
   styleUrls: ['./ngonchange.component.scss']
 })
-export class NgonchangeComponent implements OnChanges, OnInit, DoCheck {
+export class NgonchangeComponent implements OnChanges, OnInit, DoCheck, AfterContentInit {
   // for this ngonchange do not run again because this message storing the reference of the array
   // so at each append we are not changing the reference
   // @Input() message: string[];
   @Input() message: string;
-
+  @ContentChild('para') Para: ElementRef
   @ViewChild('para') temppara: ElementRef
   // ngonchanges is run only when the value of input variable is changes
   ngOnChanges(){
@@ -31,6 +31,18 @@ export class NgonchangeComponent implements OnChanges, OnInit, DoCheck {
   //ngdocheck hook is also a greate place to use, when you want to execute some code on every change detection cycle
   ngDoCheck() {
       console.log("ngdocheck is called")
+      console.log("projected content in ngdocheck", this.Para)
   }
+  
+  //ngaftercontentinit lifecycle hook is called after the components projected content has been fully initialized
+  //angular update the properties decorated with @ContentChild & @ContentChildren decorator just before this hook is raised
+  //this hook get called only once during the first change detection cycle. after that if projected content chages,this hook will not get called
+
+  
+  ngAfterContentInit() {
+      console.log("ngaftercontentinit is called")
+      console.log("projected content in ngaftercontentinit", this.Para.nativeElement)
+  }
+
   
 }
