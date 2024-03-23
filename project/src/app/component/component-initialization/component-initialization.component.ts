7a1355ad57e5f5309d3972360e84c2404ce371ae
Shubham More <shubhamdmore03@gmail.com>
2024-03-23 06:16:36 +0530

    remaining hooks

--- project/src/app/component/component-initialization/component-initialization.component.ts
+++ project/src/app/component/component-initialization/component-initialization.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-component-initialization',
@@ -7,6 +7,7 @@ import { Component, Input } from '@angular/core';
 })
 export class ComponentInitializationComponent {
   title:string = 'title'
+  toDestroy: boolean = false;
   //when the constructor is called by that time none of its input properties are updated and available to use
   // @Input() message: string = 'hello'
   // constructor(){
@@ -20,4 +21,14 @@ export class ComponentInitializationComponent {
   onClick(inputEl: HTMLInputElement){
     this.value = inputEl.value;
   }
+
+  // ngAfterViewInit() {
+  //     console.log("ngafterviewinit from component-initialization")
+  // }
+
+  handleDestroy(){
+    this.toDestroy = !this.toDestroy
+  }
+  
+  
 }
--- project/src/app/component/ngonchange/ngonchange.component.ts
+++ project/src/app/component/ngonchange/ngonchange.component.ts
@@ -1,11 +1,11 @@
-import { AfterContentChecked, AfterContentInit, Component, ContentChild, DoCheck, ElementRef, Input, OnChanges, OnInit, ViewChild } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, ContentChild, DoCheck, ElementRef, Input, OnChanges, OnDestroy, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-ngonchange',
   templateUrl: './ngonchange.component.html',
   styleUrls: ['./ngonchange.component.scss']
 })
-export class NgonchangeComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked {
+export class NgonchangeComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
   // for this ngonchange do not run again because this message storing the reference of the array
   // so at each append we are not changing the reference
   // @Input() message: string[];
@@ -14,14 +14,14 @@ export class NgonchangeComponent implements OnChanges, OnInit, DoCheck, AfterCon
   @ViewChild('para') temppara: ElementRef
   // ngonchanges is run only when the value of input variable is changes
   ngOnChanges(){
-    console.log("ngonchange occure")
+    console.log("ngonchange is called")
     // console.log(this.message)
   }
 
   //ngoninit is called only once(during the first change detection life cycle) after the ngonchange
   // any property decorated with @viewchild, @viewchildren, @contentchild or @contentchildren will not be available to use
   ngOnInit(){
-    console.log("ngoninit")
+    console.log("ngoninit is called")
     // we cannot access this
     // console.log(this.temppara.nativeElement)
   }
@@ -31,7 +31,7 @@ export class NgonchangeComponent implements OnChanges, OnInit, DoCheck, AfterCon
   //ngdocheck hook is also a greate place to use, when you want to execute some code on every change detection cycle
   ngDoCheck() {
       console.log("ngdocheck is called")
-      console.log("projected content in ngdocheck", this.Para)
+      // console.log("projected content in ngdocheck", this.Para)
   }
   
   //ngaftercontentinit lifecycle hook is called after the components projected content has been fully initialized
@@ -41,7 +41,7 @@ export class NgonchangeComponent implements OnChanges, OnInit, DoCheck, AfterCon
   
   ngAfterContentInit() {
       console.log("ngaftercontentinit is called")
-      console.log("projected content in ngaftercontentinit", this.Para.nativeElement)
+      // console.log("projected content in ngaftercontentinit", this.Para.nativeElement)
   }
 
   //ngaftercontentchecked lifecycle hook is called during every change detection cycle after angular has 
@@ -56,4 +56,25 @@ export class NgonchangeComponent implements OnChanges, OnInit, DoCheck, AfterCon
   ngAfterContentChecked() {
       console.log("ngaftercontentchecked is called")
   }
+
+  //ngafterviewinit is called after the component view template and all its child components view templates are fully initialized
+  //angular update the properties decorated with @ViewChild and @ViewChildren decorator before raising this hook
+  //this hook is called during the first change detection cycle
+  //before this hook get called all the lifecycle hook methods of its child component and directives are completely processed
+  //this component only hook
+
+  ngAfterViewInit() {
+      console.log("ngafterviewinit is called")
+      console.log("viewchild in ngafterviewinit", this.temppara)
+  }
+
+  ngAfterViewChecked() {
+      console.log("ngafterviewchecked is called")
+  }
+
+  //ngondestroy hook is called just before the component is destroyed from dom
+  ngOnDestroy() {
+      console.log("ngondestroy is called")
+  }
+
 }
